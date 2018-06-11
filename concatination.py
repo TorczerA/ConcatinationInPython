db670dcdd5d7bc873893404beb6c154694cd5db8
TorczerA <coolrohit1996@gmail.com>
2018-06-12 03:17:25 +0530

    Initial commit

---
+++ concatination.py
@@ -0,0 +1,19 @@
+#Concatination in Python
+
+print("a = Hello")
+print("b = World")
+
+a = "Hello"
+b = "World"
+
+print("c = a + b")
+
+c = a + b
+
+print("c -> ",c)
+
+print("c = a * 3")
+
+c = a * 3
+
+print("c -> ",c)
